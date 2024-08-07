83925713161c0588c90afe3bb93561de7183225e
rinzler <rinzlerdes@gmail.com>
2024-08-07 19:48:05 -0400

    Finished Exercise 4 Using Declarative Approach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,14 +3,15 @@
  * @argument {Array} targets
  */
 const removeFromArray = function (arr, ...targets) {
-  for (let i = 0; i < targets.length; i++) {
-    let targetIndex;
-    let target = targets[i];
-    while ((targetIndex = arr.findIndex((elem) => elem === target)) != -1) {
-      arr.splice(targetIndex, 1);
-    }
-  }
-  return arr;
+    return arr.filter((element) => !targets.includes(element));
+//   for (let i = 0; i < targets.length; i++) {
+//     let targetIndex;
+//     let target = targets[i];
+//     while ((targetIndex = arr.findIndex((elem) => elem === target)) != -1) {
+//       arr.splice(targetIndex, 1);
+//     }
+//   }
+//   return arr;
 };
 
 // Do not edit below this line
