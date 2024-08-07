ae28cb05406ae309525be127e60d00c22fbf3606
rinzler <rinzlerdes@gmail.com>
2024-08-07 18:24:31 -0400

    Finished Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+/**
+ * @argument {String} str
+ */
+const reverseString = function (str) {
+    return str.split('').reduce((acum, elem) => elem + acum, '');
 };
+// const reverseString = function (str) {
+//   let reversedString = '';
+
+//   for (const letter of str) {
+//     reversedString = letter + reversedString;
+//   }
+
+//   return reversedString;
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
