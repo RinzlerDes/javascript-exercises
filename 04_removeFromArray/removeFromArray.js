30bcbf17b564ee871ce277b945ec729fc93bdfd9
rinzler <rinzlerdes@gmail.com>
2024-08-07 19:30:28 -0400

    Finished Exercise 4 Using Imperative Approach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+/**
+ * @argument {Array} arr
+ * @argument {Array} targets
+ */
+const removeFromArray = function (arr, ...targets) {
+  for (let i = 0; i < targets.length; i++) {
+    let targetIndex;
+    let target = targets[i];
+    while ((targetIndex = arr.findIndex((elem) => elem === target)) != -1) {
+      arr.splice(targetIndex, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
