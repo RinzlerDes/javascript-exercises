4aeec614ab1ffe63e84ac8061cd5f1425e2ac69a
rinzler <rinzlerdes@gmail.com>
2024-08-09 16:05:38 -0400

    Finished Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+/**
+ * 
+ * @param {Number} a
+ * @param {Number} b 
+ */
+const sumAll = function(a, b) {
+    if (a < 0 
+        || b < 0
+        || isNaN(a)
+        || isNaN(b)
+        || !Number.isInteger(a)
+        || !Number.isInteger(b)
+    ) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let low = b;
+    let high = a;
+    
+    if (a < b) {
+        low = a;
+        high = b;
+    }
 
+    for (low; low <= high; low++) {
+        sum += low;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
