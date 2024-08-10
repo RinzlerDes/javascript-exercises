60f363954f90b61107b8f0b62fc87d03e7ef8fb8
rinzler <rinzlerdes@gmail.com>
2024-08-10 15:05:27 -0400

    Finished Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,33 @@
-const convertToCelsius = function() {
+/**
+ *
+ * @param {Number} temp
+ */
+const convertToCelsius = function (temp) {
+  let cel = ((temp - 32) * 5) / 9;
+
+  if (cel % 1 !== 0) {
+    cel = cel.toFixed(1);
+  }
+   
+  return Number(cel);
 };
 
-const convertToFahrenheit = function() {
+/**
+ *
+ * @param {Number} temp
+ */
+const convertToFahrenheit = function (temp) {
+  let fer = (temp * 9) / 5 + 32;
+
+  if (fer % 1 !== 0) {
+    fer = fer.toFixed(1);
+  }
+
+  return Number(fer);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
