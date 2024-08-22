f3a3e1897ff7ab22aa3c198984eb6eafc29e980e
rinzler <rinzlerdes@gmail.com>
2024-08-22 19:58:08 -0400

    Finished Fibonacci portion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+/**
+ * @param {Number} num
+    */
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if (isNaN(num)) return 'oh no';
+    else if (num < 0) return 'OOPS';
+    else if (num === 0) return 0;
+    else if (num < 3) return 1;
+
+    let sum = 1;
+    let prev = 1;
+
+    for (let i = 0; i < num - 2; i++) {
+        let temp = sum;
+        sum += prev;
+        prev = temp;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
