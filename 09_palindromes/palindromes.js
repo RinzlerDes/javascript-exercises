753bfd1543310b2d7a91b88de223f4e660f5944b
rinzler <rinzlerdes@gmail.com>
2024-08-22 12:54:51 -0400

    Finished palindrome section

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+/**
+ * @param {String} str
+    */
+const palindromes = function (str) {
+    str = str.toLowerCase();
 
+    let arr = str.split('');
+    arr = arr.filter((char) => char.match(/\w/));
+
+    str = arr.join('');
+    let str_rev = arr.reverse().join('');
+
+    return str === str_rev;
 };
 
 // Do not edit below this line
