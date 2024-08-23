9630c81e3a1a0f7712da18305a014ffe680bc98f
rinzler <rinzlerdes@gmail.com>
2024-08-23 14:34:13 -0400

    Finished getTheTitles section

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
-const getTheTitles = function() {
+/**
+ * @typedef {Object} Book
+ * @property {string} title
+ * @property {string} author
+ */
 
+
+/**
+ * @param {Book[]} books
+ */const getTheTitles = function(books) {
+    return books.map((book) => book.title);
+    // let arr = [];
+    //
+    // books.forEach((book) => {
+    //     arr.push(book.title);
+    // });
+    //
+    // return arr;
 };
 
 // Do not edit below this line
